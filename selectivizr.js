0d0da385c82758760acc4fe230fa95f81ba26b2a
Jiarong Xu <roger@fangohr.com>
2013-07-17 11:34:06 -0400

    Update selectivizr.js

--- selectivizr.js
+++ selectivizr.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
     $(".product-listing .product:nth-child(4n+4)").css("margin-right", "0px");
     $(".categories-inlay li:nth-child(4n)").css("margin-right", "0px");
+    $(".bottom-categories ul li:nth-child(4n)").css("margin-right", "0px");
+    $(".artist-page .cards .product:nth-child(3n+3)").css("margin-right", "0px");
 });
